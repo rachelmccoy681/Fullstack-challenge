738c3d5d8c7160f4e160dfdab6f54dd8a0d5987d
rachelmccoy681 <djm525@uowmail.edu.au>
2026-02-26 20:04:50 +1100

    basic react and supabase setup

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabse-js"
+
+const supabaseUrl = "https://pzjdschtolerptgnxgvk.supabase.co"
+const supabaseAnonKey = "sb_publishable_IvE5T0kwC0Tma3jcZH-SYg_a7Bsjjgj"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)

c56f357b3b1902c3303aaeab51481d250bdce6c8
rachelmccoy681 <djm525@uowmail.edu.au>
2026-02-26 21:29:27 +1100

    finalize supabase connectivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  useEffect(() =>  {
+    const fetchUsers = async () => {
+      let {data, error} = await supabase
+      .from("users")
+      .select("*")
+
+      if (error) {
+        console.log("Error fetching users:", error)
+      } else {
+        setUsers(data)
+      }
+    }
+
+    fetchUsers()
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{padding: "2rem"}}>
+        <h1>Users</h1>
+        {users.map(user => (
+          <div key={user.id} style={{border: "1px solid #ccc", marginBottom: "1rem", pading: "0.5rem"}}>
+            <h2>{user.email}</h2>
+            <p>{user.password}</p>
+
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+
+    
+      // <div>
+      //   <a href="https://vite.dev" target="_blank">
+      //     <img src={viteLogo} className="logo" alt="Vite logo" />
+      //   </a>
+      //   <a href="https://react.dev" target="_blank">
+      //     <img src={reactLogo} className="logo react" alt="React logo" />
+      //   </a>
+      // </div>
+      // <h1>Vite + React</h1>
+      // <div className="card">
+      //   <button onClick={() => setCount((count) => count + 1)}>
+      //     count is {count}
+      //   </button>
+      //   <p>
+      //     Edit <code>src/App.jsx</code> and save to test HMR
+      //   </p>
+      // </div>
+      // <p className="read-the-docs">
+      //   Click on the Vite and React logos to learn more
+      // </p>
+
+
+    // </>
   )
 }
 
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabse-js"
+import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = "https://pzjdschtolerptgnxgvk.supabase.co"
 const supabaseAnonKey = "sb_publishable_IvE5T0kwC0Tma3jcZH-SYg_a7Bsjjgj"

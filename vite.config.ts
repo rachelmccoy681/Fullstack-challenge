2cccf3bbcfaf3361c772758d7edbabec9bde05b7
rachelmccoy681 <djm525@uowmail.edu.au>
2026-02-27 11:29:38 +1100

    started the setup of tailwind and tandstack router

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    }
+  },
+
+  plugins: [
+    react(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    })
+
+
+
+  ],
+
+})
